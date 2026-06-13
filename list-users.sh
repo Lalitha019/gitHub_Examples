fb0eda882aa19961b84a46bc19cd75688874b125
Lalitha Mahalakshmi Kollipara <lalithamahalakshmi1@gmail.com>
2026-06-13 21:23:30 +0530

    Modified list-users.sh with sample output file

--- list-users.sh
+++ list-users.sh
@@ -9,10 +9,17 @@
 ################################
 
 # Function that is used to check if the user has given all the required parameters
-helper()
+function helper {
+        expected_cmd_args=2
+        if [ $# -ne $expected_cmd_args ]; then
+                echo "please execute the script with required cmd args"
+                echo "repo owner and repo name"
+		exit 1
+	fi
+        }
 
 # GitHub API URL
-API_URL="https:api.github.com"
+API_URL="https://api.github.com"
 
 # GitHub username and personal access token
 USERNAME=$username
@@ -24,19 +31,22 @@ REPO_NAME=$2
 
 # Function to make a GET request to the GitHub API
 function github_api_get {
-	local endpoint="$1"
-	local url="${API_URL}/${end_point}"
-
+	local endpoint=$1
+	# repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+	local url="${API_URL}/${endpoint}"
+	echo $url
 	# Send a GET Request to the GitHub API with autentication
-	curl -s -u "${USERNAME}:${TOKEN}" "$URL"
+	curl -s -u "${USERNAME}:${token}" "$url"
 }
 
 # Function to list users with read access to the repository
-function list_users_with_read_access{
+function list_users_with_read_access {
 	local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
-	#Fetch the list of collaborators on the repository
-	collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull ==true) | .login')"
+	# Fetch the list of collaborators on the repository
+	# collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull ==true) | .login')"
+        
+	collaborators="$(github_api_get "$endpoint")"
 
 	# Display the list of users with read access
 	if [[ -z "$collaborators" ]]; then
@@ -47,9 +57,7 @@ function list_users_with_read_access{
 	fi
 }
 
-function helper{
-	expected_cmd_args=2
-	if [ $# -ne $expected_cmd_args]; then
-		echo "please execute the script with required cmd args"
-		echo "repo owner and repo name"
-	}
+# Main
+helper "$@"
+list_users_with_read_access
+
